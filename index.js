10fae050f6515e2eb76469724d7f8ea264d08f67
MAXIME GAEL <maximejordan.gael@gmail.com>
2023-10-24 16:32:43 +0200

    Validation quete 02.1 Express

--- index.js
+++ index.js
@@ -1,56 +1,10 @@
-const express = require("express");
-const app = express();
-const port = 5001;
+const app = require('./app');
+const PORT = 5001;
 
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    colors: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    colors: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-
-const getMovies = (req, res) => {
-    res.status(200).json(movies);
-};
-app.get("/api/movies", getMovies);
-
-const favouriteMovie = (req, res) => {
-    res.send(`Welcome to my favourite movie list`);
-}
-app.get("/", favouriteMovie);
-
-
-app.get("/api/movies/:id", (req, res)=> {
-    const displayTitle = movies.find(c => c.id === parseInt(req.params.id));
-    if (!displayTitle) res.status(404).send('Not found');
-    res.send(displayTitle);
-    }
-);
-
-app.listen(port, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.error("Something bad happened");
   } else {
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on ${PORT}`);
   }
 });
